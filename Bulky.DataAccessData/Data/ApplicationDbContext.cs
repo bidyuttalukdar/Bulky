9d3cd8e34c06b2139de0b96ee9070e917f33b90e
bidyuttalukdar <bidyut3004@gmail.com>
2023-05-18 16:36:00 +0530

    code clean

--- Bulky.DataAccessData/Data/ApplicationDbContext.cs
+++ Bulky.DataAccessData/Data/ApplicationDbContext.cs
@@ -16,7 +16,7 @@ namespace Bulky.DataAccessData.Data
         {
             modelBuilder.Entity<Category>().HasData(
                     new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
-                    new Category { Id = 2, Name = "Action", DisplayOrder = 2 },
+                    new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                     new Category { Id = 3, Name = "Action", DisplayOrder = 3 }
                 );
         }
